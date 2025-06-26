29f811baf543e5ba9f3601a49d3b133e4e82882b
ThorKild_ <pro.antoine.masia@gmail.com>
2025-06-26 19:01:39 +0200

    fix: mise à jour du nom d'affichage du projet Portfolio

--- src/datas/Projects.ts
+++ src/datas/Projects.ts
@@ -8,7 +8,7 @@ export const projects: Project[] = [
       'Site portfolio développé avec Vue 3, TypeScript et Tailwind CSS. Ce site me présente, et met en avant mes compétences et projets. Présentation interactive avec système de carousel.',
     technologies: ['Vue 3', 'TypeScript', 'Tailwind', 'Shadcn'],
     gradient: 'from-blue-400 to-blue-600',
-    displayName: 'Portfolio Vue3',
+    displayName: 'Portfolio',
     githubUrl: 'https://github.com/MasiaAntoine/portfolio-vue3',
     demoUrl: 'https://masia-antoine.fr/',
   },
